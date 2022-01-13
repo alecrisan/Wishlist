a527fb441a0dd77931a08dd91c0b887b265c0208
Alexandra Bianca Crisan <alecrisan22@gmail.com>
2022-01-13 20:50:16 +0200

    Scaffolded identity pages

---
+++ Wishlist/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wishlist.Data;
+using Wishlist.Models;
+
+[assembly: HostingStartup(typeof(Wishlist.Areas.Identity.IdentityHostingStartup))]
+namespace Wishlist.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
